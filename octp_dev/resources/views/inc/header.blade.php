0167b40aa25f93ebcc222417cb9b8a56c077c08c
Proka-TP <prokic.milan@gmail.com>
2018-05-28 18:09:21 +0200

    Dodat remember_token (migracija)

--- octp_dev/resources/views/inc/header.blade.php
+++ octp_dev/resources/views/inc/header.blade.php
@@ -18,20 +18,35 @@
                 View documents
             </a>
         </li>
-        <li style="float:right" class="dropdown">
-            <a href="/login" class="dropbtn">
-                Login
-            </a>
-        </li>
-        <li style="float:right" class="dropdown">
-            <a href="/register" class="dropbtn">
-                Register
-            </a>
-        </li>
-        <li style="float:right">
-            <a href="MyProfile.html">
-                My Profile
-            </a>
-        </li>
+        @if (Auth::check())
+            <li style="float:right">
+                <a href="MyProfile.html">
+                    My Profile
+                </a>
+            </li>
+            <li style="float:right">
+                <a href="{{ route('logout') }}"
+                        onclick="event.preventDefault();
+                                 document.getElementById('logout-form').submit();">
+                                    Logout
+                </a>
+
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    {{ csrf_field() }}
+                </form>
+            </li>
+        @else
+            <li style="float:right" class="dropdown">
+                <a href="/login" class="dropbtn">
+                    Login
+                </a>
+            </li>
+            <li style="float:right" class="dropdown">
+                <a href="/register" class="dropbtn">
+                    Register
+                </a>
+            </li>
+        @endif
+
     </ul>
 </div>
