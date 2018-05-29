2528fbab5572d503628a3b882fa0a45f3044887a
in0finite <leshow816@gmail.com>
2018-05-29 19:52:03 +0200

    Working on creating new document

--- octp_dev/app/Http/Controllers/DocumentController.php
+++ octp_dev/app/Http/Controllers/DocumentController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Document;
+use App\Language;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use PhpParser\Comment\Doc;
 
 class DocumentController extends Controller
 {
@@ -27,10 +31,10 @@ class DocumentController extends Controller
     {
 
         if(!Auth::check()) {
-        //    return back()->withErrors(['You must be logged in to create new document']);
+            return back()->withErrors(['You must be logged in to create new document']);
         }
 
-        return view('document.upload');
+        return view('document.upload')->with(['languages' => Language::all()]);
     }
 
     /**
@@ -41,7 +45,74 @@ class DocumentController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // user is creating new document
+
+        if(!Auth::check()) {
+            return back()->withErrors(['You must be logged in to create new document']);
+        }
+
+        // validate input
+
+        $data = $request->all();
+
+        $validator = Validator::make($data, [
+            'typeChoice' => 'required'
+        ]);
+
+        if($validator->fails()) {
+            return redirect("/document/create")->withErrors($validator);
+        }
+
+        $choice = $data['typeChoice'];
+
+        if($choice == 'typeChoice1') {
+            // uploading document as file
+
+            $validator = Validator::make($data, [
+                'file' => 'file|max:1000000|mimes:txt'
+            ]);
+
+            if ($validator->fails()) {
+                return redirect("/document/create")->withErrors($validator);
+            }
+        } else if($choice == 'typeChoice2') {
+            // uploading document as text
+
+            $validator = Validator::make($data, [
+                'text' => 'required|string|max:1000000'
+            ]);
+
+            if ($validator->fails()) {
+                return redirect("/document/create")->withErrors($validator);
+            }
+        }
+
+        $validator = Validator::make($data, [
+            'documentName' => 'required|string|max:50',
+            'srclanguage' => 'required',
+            'dstlanguage' => 'required'
+        ]);
+
+        if($validator->fails()) {
+            return redirect("/document/create")->withErrors($validator);
+        }
+
+        // everything is ok, create new document
+
+        $document = new Document();
+        $document->date_created = Carbon::now();
+        $document->language_id = Language::all()->where('name', $data['srclanguage'])->first();
+        $document->posting_user_id = Auth::id();
+
+        // TODO: where to save text or file ? => we have to split it into sentences and add them to db
+        // TODO: document table has no column for document name
+
+        $document->save();
+
+        // TODO: create wanted translations in db
+
+        // TODO: display message to the user
+        return redirect("/document/show/" . $document->id);
     }
 
     /**
--- octp_dev/resources/views/document/upload.blade.php
+++ octp_dev/resources/views/document/upload.blade.php
@@ -6,39 +6,44 @@
 
         <div class="container">
 
-            <form action="" method="GET">
+            <form action="/document/store" method="POST">
+
                 <h1>Document upload</h1>
 
                 <p>
                     <label for="typeChoice1">File</label>
                     <input type="radio" id="typeChoice1" name="typeChoice" checked>
                 </p>
+
                 <p>
                     <label for="typeChoice2">Text</label>
                     <input type="radio" id="typeChoice2" name="typeChoice">
                 </p>
+
                 <input type="file" id="file">
+
                 <textarea id="text" cols="60" rows="20"></textarea>
+
                 <p>
                     Document name
-                    <input type="text">
+                    <input type="text" id="documentName">
                 </p>
+
                 <p>
                     Document language
                     <select name="srclanguage">
-                        <option value="Serbian">Serbian</option>
-                        <option value="English">English</option>
-                        <option value="French">French</option>
-                        <option value="German">German</option>
+                        @foreach ($languages as $language)
+                            <option value="{{ $language->name }}">{{ $language->name }}</option>
+                        @endforeach
                     </select>
                 </p>
+
                 <p>
                     Required language
                     <select name="dstlanguage">
-                        <option value="Serbian">Serbian</option>
-                        <option value="English">English</option>
-                        <option value="French">French</option>
-                        <option value="German">German</option>
+                        @foreach ($languages as $language)
+                            <option value="{{ $language->name }}">{{ $language->name }}</option>
+                        @endforeach
                     </select>
                 </p>
 
