48722a470b3d5b98c4e9e4dafb680ec57855063a
in0finite <leshow816@gmail.com>
2018-05-31 18:42:44 +0200

    Added some stats to home page

--- octp_dev/resources/views/home.blade.php
+++ octp_dev/resources/views/home.blade.php
@@ -15,6 +15,21 @@
                     @endif
 
                     You are logged in!
+
+                    <br><br><br>
+
+                    <h4>Stats</h4>
+
+                        <br>
+
+                        <p>Users: {{App\User::all()->count()}}</p>
+                        <p>Documents: {{App\Document::all()->count()}}</p>
+                        <p>Sentences: {{App\Sentence::all()->count()}}</p>
+                        <p>Languages: {{App\Language::all()->count()}}</p>
+                        <p>Translations: {{App\Translation::all()->count()}}</p>
+                        <p>Ratings: {{App\Rating::all()->count()}}</p>
+                        <p>Reports: {{App\Report::all()->count()}}</p>
+
                 </div>
             </div>
         </div>
