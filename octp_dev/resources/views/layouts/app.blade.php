051227346c7700ed4dcb0ee679fa8cf6cdf5e805
in0finite <leshow816@gmail.com>
2018-06-02 20:53:52 +0200

    Added stars to document show page

--- octp_dev/resources/views/layouts/app.blade.php
+++ octp_dev/resources/views/layouts/app.blade.php
@@ -25,7 +25,8 @@
     <!-- Styles -->
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    
+    @stack('styles')
+
 </head>
 <body>
     <div id="app">
